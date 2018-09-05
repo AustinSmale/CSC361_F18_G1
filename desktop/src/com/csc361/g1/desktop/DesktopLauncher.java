811212166b8dd7758b5eab44160a7c1bc57eea36
Austin Smale <austinsmale360@gmail.com>
2018-09-05 13:11:41 -0400

    everything else

---
+++ desktop/src/com/csc361/g1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csc361.g1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csc361.g1.CSC361_F18_G1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CSC361_F18_G1(), config);
+	}
+}
