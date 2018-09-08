db8bfae7b5736bc58f282e9d65278623c06f9caf
Austin Smale <austinsmale360@gmail.com>
2018-09-08 19:02:22 -0400

    p124 completed

--- core/src/com/csc361/g1/game/WorldRenderer.java
+++ core/src/com/csc361/g1/game/WorldRenderer.java
@@ -1,5 +1,43 @@
 package com.csc361.g1.game;
 
-public class WorldRenderer {
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+import com.csc361.g1.util.Constants;
 
+public class WorldRenderer implements Disposable{
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private WorldController worldController;
+
+	public WorldRenderer (WorldController worldController) {
+		this.worldController = worldController;
+		init();
+	}
+	
+	private void init() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+		camera.update();
+	}
+	
+	public void render() {
+		renderTestObjects();
+	}
+	
+	private void renderTestObjects() {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for(Sprite sprite: worldController.testSprites) {
+			sprite.draw(batch);
+		}
+		batch.end();
+	}
+	
+	public void resize (int width, int height) {}
+	
+	@Override public void dispose() {}
 }
