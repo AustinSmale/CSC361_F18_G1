5637951a97f954765e6d7a19f25dce8886a9871d
Austin Smale <austinsmale360@gmail.com>
2018-09-08 16:48:38 -0400

    p118 completed

--- core/src/com/csc361/g1/CanyonBunnyMain.java
+++ core/src/com/csc361/g1/CanyonBunnyMain.java
@@ -3,7 +3,7 @@ package com.csc361.g1;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.csc361.g1.game.WorldController;
 import com.csc361.g1.game.WorldRenderer;
 
@@ -27,7 +27,12 @@ public class CanyonBunnyMain implements ApplicationListener{
 		// Update game world by the time that has passed
 		// since last rendered frame.
 		worldController.upadte(Gdx.graphics.getDeltaTime());
-		// Sets the clear green sscreen color to: Cornflower Blue
+		// Sets the clear green screen color to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		// Clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// Render game world to screen
+		worldRenderer.render();
 	}
 	@Override public void resize(int width, int height) {}
 	@Override public void pause() {}
