601ac5838e304ac678e47be093984f4a6ec53213
Austin Smale <austinsmale360@gmail.com>
2018-09-08 15:55:29 -0400

    Desktop Launcher now launches correct file

--- desktop/src/com/csc361/g1/desktop/DesktopLauncher.java
+++ desktop/src/com/csc361/g1/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.csc361.g1.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.csc361.g1.CSC361_F18_G1;
+import com.csc361.g1.CanyonBunnyMain;;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_G1(), config);
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
