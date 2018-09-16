f45a47ddaa3bb4ddda8b5d67515ba6fffa060246
Allen Crigger <ac2915@cs.ship.edu>
2018-09-16 09:40:29 -0400

    p169 Completed

--- core/src/com/csc361/g1/game/objects/Rock.java
+++ core/src/com/csc361/g1/game/objects/Rock.java
@@ -11,4 +11,30 @@ import com.csc361.g1.game.Assets;
 
 public class Rock extends AbstractGameObject {
 	private TextureRegion regEdge;
+	private TextureRegion regMiddle;
+	private int length;
+	
+	//Rock Method
+	public Rock () {
+		 init();
+	}
+	
+	//Initializes the rock's features
+	private void init () {
+		dimension.set(1, 1.5f);
+		regEdge = Assets.instance.rock.edge;
+		regMiddle = Assets.instance.rock.middle;
+		//Start length of this rock
+		setLength(1);
+	}
+	
+	//Sets the length of the rock
+	public void setLength (int length) {
+		this.length = length;
+	}
+	
+	//Increases the overall length of the rock by a fixed length
+	public void increaseLength (int amount) {
+		setLength(length + amount);
+	}
 }
