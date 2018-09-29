49ade05947cc1328bb51688f2d3c73a45fc6bec9
Allen Crigger <ac2915@cs.ship.edu>
2018-09-29 11:04:57 -0400

    p246. Added to background and objects layer methods in MenuScreen Class

--- core/src/com/csc361/g1/screens/MenuScreen.java
+++ core/src/com/csc361/g1/screens/MenuScreen.java
@@ -142,6 +142,9 @@ public class MenuScreen extends AbstractGameScreen {
 	 */
 	private Table buildBackgroundLayer () {
 		Table layer = new Table();
+		// Background
+		imgBackground = new Image(skinCanyonBunny, "background");
+		layer.add(imgBackground);
 		return layer;
 	}
 	
@@ -150,6 +153,15 @@ public class MenuScreen extends AbstractGameScreen {
 	 */
 	private Table buildObjectsLayer () {
 		Table layer = new Table();
+		// Coins
+		imgCoins = new Image(skinCanyonBunny, "coins");
+		layer.addActor(imgCoins);
+		imgCoins.setPosition(135, 80);
+		
+		// Bunny
+		imgBunny = new Image(skinCanyonBunny, "bunny");
+		layer.addActor(imgBunny);
+		imgBunny.setPosition(355, 40);
 		return layer;
 	}
 	
