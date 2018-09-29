d341d874a0e3092eb9539b8c67072cb491376edf
Allen Crigger <ac2915@cs.ship.edu>
2018-09-29 10:31:53 -0400

    p242. Many vars added to MenuScreen

--- core/src/com/csc361/g1/screens/MenuScreen.java
+++ core/src/com/csc361/g1/screens/MenuScreen.java
@@ -8,9 +8,60 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 
+//UI Imports
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+//Class Imports
+import com.csc361.g1.game.Assets;
+import com.csc361.g1.util.Constants;
+
 public class MenuScreen extends AbstractGameScreen {
 
 	private static final String TAG = MenuScreen.class.getName();
+	
+	private Stage stage;
+	private Skin skinCanyonBunny;
+	
+	// Menu Stuff
+	private Image imgBackground;
+	private Image imgLogo;
+	private Image imgInfo;
+	private Image imgCoins;
+	private Image imgBunny;
+	private Button btnMenuPlay;
+	private Button btnMenuOptions;
+	 
+	// Options Stuff
+	private Window winOptions;
+	private TextButton btnWinOptSave;
+	private TextButton btnWinOptCancel;
+	private CheckBox chkSound;
+	private Slider sldSound;
+	private CheckBox chkMusic;
+	private Slider sldMusic;
+	private SelectBox selCharSkin;
+	private Image imgCharSkin;
+	private CheckBox chkShowFpsCounter;
+	 
+	// Debugging Stuff
+	private final float DEBUG_REBUILD_INTERVAL = 5.0f;
+	private boolean debugEnabled = false;
+	private float debugRebuildStage;
 	
 	/*
 	 * MenuScreen method
