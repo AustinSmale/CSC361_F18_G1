124f3a965d7ac14dea1093a5335554f38f11c304
Austin Smale <austinsmale360@gmail.com>
2018-09-29 13:16:18 -0400

    p251 completed, added missing code in constants

--- core/src/com/csc361/g1/util/Constants.java
+++ core/src/com/csc361/g1/util/Constants.java
@@ -39,4 +39,7 @@ public class Constants {
 	// Location of description file for skins
 	public static final String SKIN_LIBGDX_UI = "images/uiskin.json";
 	public static final String SKIN_CANYONBUNNY_UI = "images/canyonbunny-ui.json";
+
+	//Prefs book does not have
+	public static final String PREFERENCES = "canyonbunny.prefs";
 }
