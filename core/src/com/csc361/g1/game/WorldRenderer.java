5533a216ea42d1da686738b19aee7a6c94951d32
Allen Crigger <ac2915@cs.ship.edu>
2018-09-30 00:08:25 -0400

    Score visual code added.

--- core/src/com/csc361/g1/game/WorldRenderer.java
+++ core/src/com/csc361/g1/game/WorldRenderer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 import com.csc361.g1.util.Constants;
@@ -102,6 +103,18 @@ public class WorldRenderer implements Disposable {
 		float y = -15;
 		batch.draw(Assets.instance.goldCoin.goldCoin, x, y, 50, 50, 100, 100, 0.35f, -0.35f, 0);
 		Assets.instance.fonts.defaultBig.draw(batch, "" + worldController.score, x + 75, y + 37);
+		
+		//Score visual effect.
+		float offsetX = 50;
+		float offsetY = 50;
+		if (worldController.scoreVisual < worldController.score) {
+			long shakeAlpha = System.currentTimeMillis() % 360;
+			float shakeDist = 1.5f;
+			offsetX += MathUtils.sinDeg(shakeAlpha * 2.2f) * shakeDist;
+			offsetY += MathUtils.sinDeg(shakeAlpha * 2.9f) * shakeDist;
+		}
+		batch.draw(Assets.instance.goldCoin.goldCoin, x, y, offsetX, offsetY, 100, 100, 0.35f, -0.35f, 0);
+		Assets.instance.fonts.defaultBig.draw(batch, "" + (int)worldController.scoreVisual, x + 75, y + 37);
 	}
 
 	/**
