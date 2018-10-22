67d039dee9c6dd279f6da1f8906385179325ef14
Allen Crigger <ac2915@cs.ship.edu>
2018-10-22 16:18:45 -0400

    Animation Added to GoldCoin

--- core/src/com/csc361/g1/game/objects/GoldCoin.java
+++ core/src/com/csc361/g1/game/objects/GoldCoin.java
@@ -8,6 +8,8 @@ package com.csc361.g1.game.objects;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.csc361.g1.game.Assets;
+//Chapter 12
+import com.badlogic.gdx.math.MathUtils;
 
 public class GoldCoin extends AbstractGameObject {
 	private TextureRegion regGoldCoin;
@@ -21,7 +23,10 @@ public class GoldCoin extends AbstractGameObject {
 	//init Method creates the properties of the coin
 	private void init () {
 		dimension.set(0.5f, 0.5f);
-		regGoldCoin = Assets.instance.goldCoin.goldCoin;
+		//regGoldCoin = Assets.instance.goldCoin.goldCoin;
+		
+		setAnimation(Assets.instance.goldCoin.animGoldCoin);
+		stateTime = MathUtils.random(0.0f, 1.0f);
 		
 		// Set bounding box for collision detection
 		bounds.set(0, 0, dimension.x, dimension.y);
@@ -32,7 +37,8 @@ public class GoldCoin extends AbstractGameObject {
 	public void render (SpriteBatch batch) {
 		if (collected) return;
 		TextureRegion reg = null;
-		reg = regGoldCoin;
+		//reg = regGoldCoin;
+		reg = reg = animation.getKeyFrame(stateTime, true);
 		batch.draw(reg.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y,
 		rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
 	}
